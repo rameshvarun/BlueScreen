2a56ba4dd4004fbbb58c0d10944bb20e9844dfa9
Varun Ramesh <varun.ramesh@aol.com>
2014-08-24 00:22:17 -0700

    Intro, ending, some glue

---
+++ Assets/GameEnd.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameEnd : MonoBehaviour {
+
+	private float timer = 0.0f;
+	
+	public Font font;
+	GUIStyle style = new GUIStyle();
+
+
+	// Use this for initialization
+	void Start () {
+		style.font = font;
+		style.fontSize = 30;
+		style.normal.textColor = Color.white;
+		style.alignment = TextAnchor.MiddleCenter;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+	}
+
+	void OnGUI() {
+		Rect position = new Rect(0, 0, Screen.width, Screen.height);
+		
+		if(timer < 4.0f) {
+			
+		}
+		/*else if(timer < 9.0f) {
+			if(!audio.isPlaying)
+				audio.Play();
+
+			GUI.Label(position, "Guess I'll live to see another das", style);
+		}*/
+		else {
+			if(!audio.isPlaying)
+				audio.Play();
+
+			GUI.Label(position, "Thanks for playing", style);
+		}
+	}
+}
---
+++ Assets/IntroText.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroText : MonoBehaviour {
+
+	private float timer = 0.0f;
+
+	public Font font;
+	GUIStyle style = new GUIStyle();
+
+	public int level;
+
+
+	// Use this for initialization
+	void Start () {
+		style.font = font;
+		style.fontSize = 30;
+		style.normal.textColor = Color.white;
+		style.alignment = TextAnchor.MiddleCenter;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+	}
+
+	void OnGUI() {
+		Rect position = new Rect(0, 0, Screen.width, Screen.height);
+		
+		if(timer < 1.0f) {
+
+		}
+		else if(timer < 6.0f) {
+			GUI.Label(position, "I helped them take over the city.", style);
+		}
+		else if(timer < 11.0f) {
+			GUI.Label(position, "Then, I was a loose end.", style);
+		}
+		else if(timer > 14.0f) {
+			Application.LoadLevel(level);
+		}
+	}
+}
