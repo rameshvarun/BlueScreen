c812bf72fbbffa4312deecdb46543c6448f85c8a
Varun Ramesh <varunramesh@live.com>
2021-11-28 22:14:49 -0800

    Adjustments

--- Scripts/build-release.bat
+++ Scripts/build-release.bat
@@ -4,4 +4,4 @@ set WIN64=-buildWindows64Player ./Release/blue-screen-win/BlueScreen.exe
 set OSX=-buildOSXUniversalPlayer ./Release/blue-screen-osx/BlueScreen.app
 set LINUX64=-buildLinux64Player ./Release/blue-screen-linux/BlueScreen.x86_64
 
-"%PROGRAMFILES%\Unity\Hub\Editor\2017.4.40f1\Editor\Unity.exe" -batchmode -quit -projectPath %PROJECT_PATH% %WIN64% %OSX% %LINUX64%
+"%PROGRAMFILES%\Unity\Hub\Editor\2020.3.22f1\Editor\Unity.exe" -batchmode -quit -projectPath %PROJECT_PATH% %WIN64% %OSX% %LINUX64%
